86b5d7f20df4474ce93c9b070c9683974f4d1dab
nhandt <dtnhan.ete.dut@gmail.com>
2023-04-25 07:54:34 +0700

    complete file transfer

--- App/views.py
+++ App/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+import shutil
+import os
+from pathlib import Path
+from os.path import exists
+from django.core.files.storage import FileSystemStorage
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from django.http import FileResponse
 
-# Create your views here.
+from .models import Item
+from .serializers import ItemSerializer
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+@api_view(["POST"])
+def getParamsFile(request):
+    # get file param .h5 with key = 'file' sent from center 
+    file_param = request.FILES['file']
+
+    # use this for save that file param .h5
+    fs = FileSystemStorage()
+
+    # path of file param .h5 in module AI
+    file_param_path = BASE_DIR.joinpath('module_ai/wpod-net.h5')
+
+    # path of file that will saved in
+    src_path = BASE_DIR.joinpath('wpod-net.h5')
+
+    # path of file that will be moved in to module AI
+    module_path = BASE_DIR.joinpath('module_ai')
+
+    # check that file param h5 has already been in module ai, delete that if exist
+    if exists(file_param_path):
+        os.remove(file_param_path)
+
+    # save file .h5 in src_path
+    fs.save(file_param.name, file_param)
+    
+    # move that file to file_param_path
+    shutil.move(src_path, module_path)
+
+    # TODO apply file .h5 and train that then response file param .h5 back to center
+    # TODO train process()
+    file_response = FileResponse(open(file_param_path), 'rb')
+
+    response = FileResponse(file_response)
+
+    return response
+    
+
+
--- RestFramework/urls.py
+++ RestFramework/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("App.urls"))
+    path('api/', include("App.urls"))
 ]
 
